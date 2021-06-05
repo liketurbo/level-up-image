c0010a1c40ab5cb0fd6f39cda40d77a87889f992
Ramazan Chasygov <theliketurbo@gmail.com>
2021-06-05 19:20:40 +0300

    feat: add icon to link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons"
 import { Flex, Link, Text } from "@chakra-ui/react"
 import { useTranslation } from "next-i18next"
 
@@ -7,8 +8,8 @@ export default function Footer() {
   return (
     <Flex justifyContent="center">
       <Text mr={1}>{t("powered-by")}</Text>
-      <Link href="https://letsenhance.io" isExternal>
-        Letsenhance
+      <Link href="https://letsenhance.io" color="teal.500" isExternal>
+        Letsenhance <ExternalLinkIcon ml="2px" />
       </Link>
     </Flex>
   )
